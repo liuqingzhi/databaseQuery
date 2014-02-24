32c37296ee08a8ce0777d99c9622142d3b952362
liuqingzhi <liuqingzhi@haijie..com>
2014-02-24 18:04:29 +0800

    dto中增加redisTemplate对象

--- src/main/java/com/yesmynet/query/core/dto/RedisConfig.java
+++ src/main/java/com/yesmynet/query/core/dto/RedisConfig.java
@@ -1,15 +1,30 @@
 package com.yesmynet.query.core.dto;
 
+import org.springframework.data.redis.core.RedisTemplate;
+
 public class RedisConfig extends BaseDto{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1612585674754711295L;
 	/**
 	 * 显示名称
 	 */
 	private String name;
-	//private RedisTemplate redisTemplate;
+	/**
+	 * 操作redis的对象
+	 */
+	private RedisTemplate redisTemplate;
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	public RedisTemplate getRedisTemplate() {
+		return redisTemplate;
+	}
+	public void setRedisTemplate(RedisTemplate redisTemplate) {
+		this.redisTemplate = redisTemplate;
 	}
 }
