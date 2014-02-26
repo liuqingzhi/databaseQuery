4ae5379658be7899659df639fee9e548d52d7996
liuqingzhi <liuqingzhi@haijie.com>
2014-02-26 23:31:47 +0800

    开始做参数的显示布局

--- src/main/java/com/yesmynet/query/http/service/impl/FormLayoutService.java
+++ src/main/java/com/yesmynet/query/http/service/impl/FormLayoutService.java
@@ -23,12 +23,93 @@ public class FormLayoutService implements ParameterLayoutService {
 	 * 是否显示参数的标题
 	 */
 	private boolean showTitle;
+	/**
+	 * 表示一个参数在table中的位置
+	 * @author liuqingzhi
+	 *
+	 */
+	private class ParameterPosition
+	{
+		/**
+		 * 参数
+		 */
+		private Parameter parameter;
+		/**
+		 * 计算后参数所在的行
+		 */
+		private int row;
+		/**
+		 * 计算后参数所在的列
+		 */
+		private int column;
+		/**
+		 * 计算后的行跨
+		 */
+		private int rowSpan;
+		/**
+		 * 计算后的列跨
+		 */
+		private int columnSpan;
+		public Parameter getParameter() {
+			return parameter;
+		}
+		public void setParameter(Parameter parameter) {
+			this.parameter = parameter;
+		}
+		public int getRow() {
+			return row;
+		}
+		public void setRow(int row) {
+			this.row = row;
+		}
+		public int getColumn() {
+			return column;
+		}
+		public void setColumn(int column) {
+			this.column = column;
+		}
+		public int getRowSpan() {
+			return rowSpan;
+		}
+		public void setRowSpan(int rowSpan) {
+			this.rowSpan = rowSpan;
+		}
+		public int getColumnSpan() {
+			return columnSpan;
+		}
+		public void setColumnSpan(int columnSpan) {
+			this.columnSpan = columnSpan;
+		}
+		
+	}
 	@Override
 	public String showParameter(List<Parameter> parameters)
 	{
 		List<Parameter> sort = sort(parameters);
+		List<ParameterPosition> parameterPositions = computePosition(sort);
+		String html = getHtml(parameterPositions);
 		
-		return null;
+		return html;
+	}
+	/**
+	 * 计算出每个参数所在的位置 
+	 * @param parameters
+	 * @return
+	 */
+	private List<ParameterPosition> computePosition(List<Parameter> parameters)
+	{
+		List<ParameterPosition> re=new ArrayList<ParameterPosition>();
+		return re;
+	}
+	/**
+	 * 得到显示参数的html
+	 * @param parameters
+	 * @return
+	 */
+	private String getHtml(List<ParameterPosition> parameters)
+	{
+		StringBuilder re=new StringBuilder();
+		return re.toString();
 	}
 	/**
 	 * 把参数按显示顺序排序
